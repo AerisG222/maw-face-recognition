f14dddc59bc181bc2210ed49e7fc2d2fea5d251b
Mike Morano <mmorano@mikeandwan.us>
2021-07-25 09:36:59 -0400

    also mount the test directory

--- tools/enter_dev_container.sh
+++ tools/enter_dev_container.sh
@@ -8,6 +8,7 @@ podman run \
     -it \
     --rm \
     -v "${src}/src":/src \
+    -v "${src}/test":/test \
     -v "${facedb}":/facedb \
     -v "${facetests}":/facetests \
     --hooks-dir=/usr/share/containers/oci/hooks.d/ \
