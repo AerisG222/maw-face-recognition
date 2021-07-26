a3cd88f3d6e17223020b0a877e76d0ce2648418a
Mike Morano <mmorano@mikeandwan.us>
2021-07-26 10:53:25 -0400

    some initial work where faces were matched

--- src/face_recognition.py
+++ src/face_recognition.py
@@ -1,4 +1,33 @@
+import os
+import cv2
+from retinaface import RetinaFace
 from deepface import DeepFace
 
-result = DeepFace.verify("/facedb/wan_choi/wan1.jpg", "/facetests/Z62_2341.jpg")
-print(result)
+detection_models = ["opencv", "ssd", "dlib", "mtcnn", "retinaface"]
+recognition_models = ["VGG-Face", "Facenet", "Facenet512", "OpenFace", "DeepFace", "DeepID", "ArcFace", "Dlib"]
+
+detector = detection_models[4]
+recognizer = recognition_models[6]
+
+recognizer_model = DeepFace.build_model(recognizer)
+
+for root, dirs, files in os.walk('/facetests'):
+    for file in files:
+        imgfile = os.path.join(root, file)
+
+        print(f'Processing {imgfile}...')
+
+        faces = RetinaFace.extract_faces(imgfile)
+
+        print(f'  - found {len(faces)} face(s)')
+
+        i = 1
+
+        for face in faces:
+            #cv2.imwrite(f'out{i}_{file}', face)
+            print(f'  - trying to identify face {i}')
+
+            found_face = DeepFace.find(face, '/facedb', model = recognizer_model, detector_backend = detector, enforce_detection = False)
+            print(found_face)
+
+            i = i + 1
