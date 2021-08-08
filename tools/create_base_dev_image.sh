1fe4e587ea6990b7d1b6c3b9f9ca383fc50ae24e
Mike Morano <mmorano@mikeandwan.us>
2021-08-08 08:59:11 -0400

    update image buld process to download files into a container rather than local pc

--- tools/create_base_dev_image.sh
+++ tools/create_base_dev_image.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-./download_weights.sh
-
 buildah bud -f "${SCRIPT_DIR}/Containerfile" -t maw-facerec "${SCRIPT_DIR}"
