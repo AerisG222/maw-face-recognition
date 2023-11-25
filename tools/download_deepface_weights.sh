cefb31755c1ef9ec36a3866336619644bb9d729f
Mike Morano <mmorano@mikeandwan.us>
2023-11-25 17:15:39 -0500

    add another model download, as this would otherwise be downloaded on each run

--- tools/download_deepface_weights.sh
+++ tools/download_deepface_weights.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 MODEL_DIR="${SCRIPT_DIR}/faces_model_weights"
-MODELS=(
+SERENGIL_MODEL_BASE_URL="https://github.com/serengil/deepface_models/releases/download/v1.0"
+SERENGIL_MODELS=(
     "age_model_weights.h5"
     "arcface_weights.h5"
     "deepid_keras_weights.h5"
@@ -21,11 +22,16 @@ fi
 
 pushd "${MODEL_DIR}"
 
-for MODEL in ${MODELS[@]}
+for MODEL in ${SERENGIL_MODELS[@]}
 do
     if [ ! -f "${MODEL}" ]; then
-        curl -JLO "https://github.com/serengil/deepface_models/releases/download/v1.0/${MODEL}"
+        curl -JLO "${SERENGIL_MODEL_BASE_URL}/${MODEL}"
     fi
 done
+
+# this ends up getting referenced by the vggface2 model, so pull this down ahead of time
+curl -JLO "https://github.com/swghosh/DeepFace/releases/download/weights-vggface2-2d-aligned/VGGFace2_DeepFace_weights_val-0.9034.h5.zip"
+unzip VGGFace2_DeepFace_weights_val-0.9034.h5.zip
+rm VGGFace2_DeepFace_weights_val-0.9034.h5.zip
 
 popd
