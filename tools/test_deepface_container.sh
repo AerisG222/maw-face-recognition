ed0603e0ed7e875e306341fdadcead75265d6546
Mike Morano <mmorano@mikeandwan.us>
2023-11-26 17:05:00 -0500

    add comment on different ways to test the custom container

--- tools/test_deepface_container.sh
+++ tools/test_deepface_container.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# :: ways to test the custom container ::
+
+# 1. confirm tensorflow will use gpu:
+# python -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
+
+# 2. run test of our script
+# python /mnt/scripts/main.py /mnt/facedb /mnt/images /mnt/results/out1.csv
+
 podman run \
     -it \
     --rm \
