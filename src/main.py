f1c661335e9029720a2c564f415db061500b24d5
Mike Morano <mmorano@mikeandwan.us>
2023-11-26 17:13:25 -0500

    parallelize recognition and also filter results down to the most likely matches for each face in a photo

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
+import concurrent.futures
 import os
 import sys
+import time
 import pandas as pd
 import facerec
 
@@ -30,31 +32,54 @@ def verify_args(faceDbDir, inputDir, outfile):
     return isValid
 
 def addInputImageToDataFrame(file, df):
-    df['Photo'] = file
+    df['photo'] = file
 
-def runDetection(faceDbDir, inputDir, outfile):
+def runDetection(file, faceDbDir):
+    df = facerec \
+        .recognizeFaces(file, faceDbDir)
+
+    if df.size == 0:
+        return pd.DataFrame()
+
+    addInputImageToDataFrame(file, df)
+
+    # matches with index 0 are the best match
+    # we pass [0] rather than 0 to identify the best cases so that when there is
+    # only one unique face identified (with potentially many matches), we always return a dataframe
+    # which is expected by our caller(s)
+    return df.loc[[0]]
+
+def runDetections(faceDbDir, inputDir, outfile):
     resultDf = pd.DataFrame()
 
+    print(inputDir)
+
+    start_time = time.time()
+
     for root, dirs, files in os.walk(inputDir):
-        for file in files:
-            df = facerec \
-                .recognizeFaces(os.path.join(root, file), faceDbDir) \
-                .filter(like="0", axis=0)
+        print(f"Processing photos in directory: {root}")
 
-            # TODO: hmm, the line below removes the identity (matched file from db), rather than the zero at the start...
-            # ignore first column, which just contains 0 - representing the index for the best matching face
-            # df.drop(columns=df.columns[0], axis=1, inplace=True)
+        filenames = list(map(lambda f: f"{root}/{f}", files))
+        faceDbArgs = [faceDbDir] * len(filenames)
 
-            addInputImageToDataFrame(os.path.join(root, file), df)
+        # manually testing showed that 2 is optimal:
+        #  - 1 worker  -> 29.5s
+        #  - 2 workers -> 21.5s
+        #  - 3 workers -> 23.2s
+        #  - 4 workers -> 26.5s
+        #  - 8 workers -> 45.2s
+        with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+            for file, df in zip(filenames, executor.map(runDetection, filenames, faceDbArgs)):
+                if resultDf.empty:
+                    resultDf = df
+                elif not df.empty:
+                    resultDf = pd.concat([resultDf, df])
 
-            if resultDf.empty:
-                resultDf = df
-            elif not df.empty:
-                resultDf = pd.concat([resultDf, df])
+    print("--- %s seconds ---" % (time.time() - start_time))
 
     print(resultDf.to_csv())
 
-def main():
+if __name__ == '__main__':
     if len(sys.argv) != 4 :
         show_usage()
         exit(1)
@@ -66,6 +91,4 @@ def main():
     if not verify_args(faceDbDir, inputDir, outfile):
         exit(2)
 
-    runDetection(faceDbDir, inputDir, outfile)
-
-main()
+    runDetections(faceDbDir, inputDir, outfile)
