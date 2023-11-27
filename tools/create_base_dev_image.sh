1e9911421716054f304cdd946724f0b6732c586c
Mike Morano <mmorano@mikeandwan.us>
2023-11-27 08:38:22 -0500

    we are only using this container locally, expect weights to already be downloaded to simplify and improve container build performance

--- tools/create_base_dev_image.sh
+++ tools/create_base_dev_image.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+MODEL_DIR="${SCRIPT_DIR}/faces_model_weights"
+
+if [ ! -d "${MODEL_DIR}" ]; then
+    echo "Please run download_deepface_weights.sh first!"
+    exit
+fi
 
 buildah bud -f "${SCRIPT_DIR}/Containerfile" -t maw-facerec "${SCRIPT_DIR}"
